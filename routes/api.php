25f604cbe9a0a1d3c45c0da09ed0c888cdbbe002
Curtis Delicata <curtis@liberugroup.com>
2026-08-29 19:30:48 +0100

    Synchronize maintenance module
    Merge the verified maintenance module synchronization.

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::middleware('auth:sanctum')->prefix('api/v1/maintenance/scheduling')->grou
     Route::post('/', [ScheduleEntryController::class, 'store']);
     Route::get('/{scheduleEntry}', [ScheduleEntryController::class, 'show']);
     Route::patch('/{scheduleEntry}', [ScheduleEntryController::class, 'update']);
+    Route::post('/{scheduleEntry}/transitions', [ScheduleEntryController::class, 'transition']);
     Route::delete('/{scheduleEntry}', [ScheduleEntryController::class, 'destroy']);
 });
